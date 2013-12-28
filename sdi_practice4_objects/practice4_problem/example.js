02ad7d101c05b2adf651c004f97076a56c664eef
Richard Hutchinson <hutchinsonrichard1@gmail.com>
2013-12-27 21:10:28 -0500

    update

---
+++ sdi_practice4_objects/practice4_problem/example.js
@@ -0,0 +1,10 @@
+var toyota = "Toyota";
+
+//note: this is an object literal
+var car = {
+	make: Toyota,
+	color: "Red",
+	year: "1988"
+};
+
+console.log(car);
---
+++ sdi_practice4_objects/practice4_solution/example.js
@@ -0,0 +1,10 @@
+var toyota = "Toyota";
+
+//note: this is an object literal
+var car = {
+	make: toyota,
+	color: "Red",
+	year: "1988"
+};
+
+console.log(car);
