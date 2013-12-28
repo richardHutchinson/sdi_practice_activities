dfabbe4468177e1db5ca1b4436b5894802bf5f92
Richard Hutchinson <hutchinsonrichard1@gmail.com>
2013-12-27 21:37:38 -0500

    updates

---
+++ sdi_practice5_json/practice5_solution/example.js
@@ -0,0 +1,29 @@
+var automobile = {
+	"cars": [
+		{
+			make: "Toyota Camry",
+			color: "Red",
+			year: "1988"
+		},
+		{
+			make: "Ford Focus",
+			color: "Blue",
+			year: "1999"
+		}
+	],
+	"trucks": [
+		{
+			make: "Toyota Tundra",
+			color: "Red",
+			year: "1988"
+		},
+		{
+			make: "Ford F-150",
+			color: "Blue",
+			year: "1999"
+		}
+	]
+};
+
+console.log(automobile);
+console.log(automobile.cars[0].make);
